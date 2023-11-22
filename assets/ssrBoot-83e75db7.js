a58d402a0d8dd0fe8c65e80aeb6018d77556397c
okerefe <okerefe@gmail.com>
2023-11-22 14:34:21 +0100

    update

--- assets/ssrBoot-83e75db7.js
+++
@@ -1,1 +0,0 @@
-import{s,b4 as t,l as e,bf as n}from"./index-d2f22596.js";function i(){const o=s(!1);return t(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:e(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
